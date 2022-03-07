19acda2e40a161224e8a241c061103b645fd50dc
MG2496 <michaelgamboamoya24@gmail.com>
2022-03-06 22:11:38 -0600

    completed

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Table from './Table';
+
+
+class App extends React.Component {
+  state ={
+    characters: [
+      {
+        name: 'John',
+        job: 'Smithery'
+      },
+      {
+        name: 'Clyde',
+        job: 'Carpenter'
+      },
+      {
+        name: 'Wanda',
+        job: 'Engineer'
+      },
+      {
+        name: 'Claudia',
+        job: 'Manager'
+      },
+    ],
+  };
+
+  removeCharacter = index => {
+    const { characters } = this.state;
+    this.setState({
+      characters: characters.filter((char, i) => {
+        return i !== index;
+      }),
+    });
+  };
+  render(){
+    const { characters }  = this.state;
+
+    return (
+      <div className="container">
+        <Table characters={characters} removeCharacter={this.removeCharacter} />
+      </div>
+    );
+  }
+}
+
+export default App;
